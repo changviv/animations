8a64b70e03adb83aae40ace862487184ecb57e29
Vivien <vchang103@gmail.com>
2023-12-04 16:35:49 -0500

    marquee button basic animation

---
+++ src/MarqueeButton.tsx
@@ -0,0 +1,46 @@
+import { Button, Text, keyframes } from "@chakra-ui/react";
+import { useState } from "react";
+// import "./MarqueeButton.css";
+
+const MarqueeButton = (): JSX.Element => {
+  const [onHover, setOnHover] = useState(false);
+  const marquee = keyframes`
+    from { transform: translateX(0%); }
+    to { transform: translateX(-120%); }
+  `;
+
+  const animation = onHover ? `${marquee} infinite 4s linear` : undefined;
+
+  return (
+    <Button
+      onMouseEnter={() => setOnHover(true)}
+      onMouseLeave={() => setOnHover(false)}
+      overflowX="hidden"
+      transition={"all .4s ease-out"}
+      position={"relative"}
+    >
+      <Text
+        _after={{
+          position: "absolute",
+          left: " 130%",
+          width: "100%",
+          textAlign: "center",
+        }}
+        animation={animation}
+      >
+        Stack Sorted
+      </Text>
+      <Text
+        position={"absolute"}
+        left="120%"
+        w="100%"
+        textAlign={"center"}
+        animation={animation}
+      >
+        Stack Sorted
+      </Text>
+    </Button>
+  );
+};
+
+export default MarqueeButton;
